028342fdeccb6b28ea4650210c20bf32304939b1
Pavan <pavandesh2004@gmail.com>
2026-08-13 12:22:36 +0530

    Add gitignore and remove transient build/cache files

--- test_emulator.py
+++
@@ -1,8 +0,0 @@
-import os
-from google.cloud import firestore
-
-print("FIRESTORE_EMULATOR_HOST:", os.environ.get("FIRESTORE_EMULATOR_HOST"))
-db = firestore.Client(project="intern-bnmit-july-2026")
-doc = db.collection("test").document("ping")
-doc.set({"message": "Hello Firestore Emulator!", "status": "active"})
-print("Document retrieved from emulator:", doc.get().to_dict())
